0fab0759d4ef7d33ff89ff641d1331a9c3df2054
DitaNurA <79654266+DitaNurA@users.noreply.github.com>
2021-03-20 23:42:20 +0700

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+    <title>Home</title>
+    <h1>{{ $name }}</h1>
+    <p class="h2">Halaman Home</p> <a href="https://alowisata.com/"><button>Home</button></a>
+    </body>
+</html>
